04a759ad8ed26d26dddc5b6d8cd47d5e53cc27a6
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-05-11 02:26:26 -0600

    mejor funcionalidad de cambiar el color

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,11 +121,25 @@ let swiper = new Swiper(".portfolio__container", {
 /*==================== DARK LIGHT THEME ====================*/ 
 
 
+// const colorPicker = document.getElementById('colorpicker');
+// const root = document.documentElement;
+
+// colorPicker.addEventListener('input', (e) => {
+//   const color = e.target.value;
+//   const hue = parseInt(color.substring(1, 3), 16);
+//   root.style.setProperty('--hue-color', hue);
+// });
 const colorPicker = document.getElementById('colorpicker');
-const root = document.documentElement;
+const colorPickerBtn = document.querySelector('.color__picker');
+
+colorPickerBtn.addEventListener('click', () => {
+  colorPicker.click();
+});
 
 colorPicker.addEventListener('input', (e) => {
   const color = e.target.value;
   const hue = parseInt(color.substring(1, 3), 16);
-  root.style.setProperty('--hue-color', hue);
+  document.documentElement.style.setProperty('--hue-color', hue);
 });
+
+
